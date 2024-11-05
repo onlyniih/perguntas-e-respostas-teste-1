81ac394844d8561a7cdd4f14af2641a4b57bc797
onlyniih <ncristine002@gmail.com>
2024-11-05 08:12:48 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'O que é o que é',
+    'Cheia de furinhos mas consegue reter água?',
+    'A esponja'
+)
+
+criaCartao(
+    'O que é o que é',
+    'Quanto mais eu tiro, mais eu tenho',
+    'Fotografias'
+)
+
+criaCartao(
+    'O que é o que é',
+    'Qual o queijo que mais sente dor?',
+    'O queijo ralado'
+)
+
+criaCartao(
+    'O que é o que é',
+    'Uma pergunta que você nunca pode responder com sim?',
+    'Você está dormindo?'
+)
